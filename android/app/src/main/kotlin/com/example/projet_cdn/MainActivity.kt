0e172cc3c377d0380092ff273ce8bdb554ae6b04
DiegoAxSanchez <pajarlalo@gmail.com>
2022-09-19 18:45:25 -0400

    flutter-login-screen-firebase-auth-facebook-login

---
+++ android/app/src/main/kotlin/com/example/projet_cdn/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.projet_cdn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/auth/launcherScreen/launcher_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:projet_cdn/constants.dart';
+import 'package:projet_cdn/services/helper.dart';
+import 'package:projet_cdn/ui/auth/authentication_bloc.dart';
+import 'package:projet_cdn/ui/auth/onBoarding/on_boarding_screen.dart';
+import 'package:projet_cdn/ui/auth/welcome/welcome_screen.dart';
+import 'package:projet_cdn/ui/home/home_screen.dart';
+
+class LauncherScreen extends StatefulWidget {
+  const LauncherScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LauncherScreen> createState() => _LauncherScreenState();
+}
+
+class _LauncherScreenState extends State<LauncherScreen> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<AuthenticationBloc>().add(CheckFirstRunEvent());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(COLOR_PRIMARY),
+      body: BlocListener<AuthenticationBloc, AuthenticationState>(
+        listener: (context, state) {
+          switch (state.authState) {
+            case AuthState.firstRun:
+              pushReplacement(context, const OnBoardingScreen());
+              break;
+            case AuthState.authenticated:
+              pushReplacement(context, HomeScreen(user: state.user!));
+              break;
+            case AuthState.unauthenticated:
+              pushReplacement(context, const WelcomeScreen());
+              break;
+          }
+        },
+        child: const Center(
+          child: CircularProgressIndicator.adaptive(
+            backgroundColor: Colors.white,
+            valueColor: AlwaysStoppedAnimation(Color(COLOR_PRIMARY)),
+          ),
+        ),
+      ),
+    );
+  }
+}
